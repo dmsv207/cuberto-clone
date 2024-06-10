9abf3c53de9caa76719094ebe43deecb64dfb3ff
dikshitME <dikshit.me.23@nitj.ac.in>
2024-06-11 02:45:44 +0530

    new commit

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
-Shery.mouseFollower({
-    skew: true,
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
-  });
- Shery.makeMagnet(".magnet",);
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
 
- Shery.hoverWithMediaCircle(".hvr", {
-    videos: ["0.mp4", "2.mp4","3.mp4"],
-  });
-//   Shery.imageMasker("#magnet" , {
-
-//     mouseFollower: true,
-//     text: "",
-//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-//     duration: 1,
-//   });
-  
+gsap.to(".fleftelem", {
+  scrollTrigger: {
+    trigger: "#featured-content",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    endTrigger: ".last",
+    scrub: 1,
+  },
+  y: "-300%",
+  ease: Power1,
+});
 
 
+var sections = document.querySelectorAll(".fleftelem");
+Shery.imageEffect(".images", {
+  style: 4,
+  config: { onMouse: { value: 1 } },
+  slideStyle: (setScroll) => {
+    sections.forEach(function (section, index) {
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top 10%",
+        scrub: 1,
+        onUpdate: function (prog) {
+          setScroll(prog.progress + index);
+        },
+      });
+    });
+  },
+});
 
 
 
 
-  Shery.textAnimate(".hvr", {
-    style: 1,
-    y: 10,
-    delay: 0.1,
-    duration: 2,
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    multiplier: 0.1,
-  });
 
 
 
-  Shery.imageEffect(".img", {
-    style: 6,
-    debug: true,
-  });
+
+Shery.hoverWithMediaCircle(".main-c", {
+  videos: ["0.mp4","2.mp4", "3.mp4"],
+});
